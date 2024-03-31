e49e62eb2417f0a8fce63c09da263ae088f0a02d
Natália Korsack <natalia-rosa2@educar.rs.gov.br>
2024-03-31 14:44:55 -0300

    atualização 31/03

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 /* ===================== PRINCIPAIS OBJETOS  =================================*/
 
-let addNote = document.querySelector('#add-note');//Botão de para adicionar nota
-let btnCloseModal =  document.querySelector('#btn-close-modal'); //fechar janela modal com os detalhes da nota.
-let modal = document.querySelector('#modal'); //Modal para edição das notas
-let modalView = document.querySelector('#modal-view'); //Modal para exibição dos detalhes da nota
+let addNote = document.querySelector('#add-note'); //Botão de para adicionar nota
+let btnCloseModal =  document.querySelector('#btn-close-modal');//fechar janela modal com os detalhes da nota.
+let btnEditNote = document.querySelector('#btn-edit-note'); //edição de uma nota
+let btnExcludeNote = document.querySelector('#btn-exclude-note'); //exclusão de nota 
+let modal = document.querySelector('#modal');//Modal para edição das notas
+let modalView = document.querySelector('#modal-view');  //Modal para exibição dos detalhes da nota
 let notes = document.querySelector('#notes');//Lista divs com dados das notas
 let btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
 let btnCloseNote = document.querySelector("#btn-close-note"); //icone para fechar modal de edição de nota.
 
 
 /* ===================== EVENTOS  =================================*/
+
 addNote.addEventListener("click", (evt) =>{
     evt.preventDefault();
     modal.style.display = "block";
@@ -39,7 +42,18 @@ btnCloseNote.addEventListener("click", (evt) =>{
     notes.style.display = "flex";
     addNote.style.display = "block";
 
-})
+});
+
+btnEditNote.addEventListener("click", (evt) => {
+    evt.preventDefault();
+
+
+});
+
+btnExcludeNote.addEventListener("click", (evt) => {
+   evt.preventDefault();
+
+  });
 
 /* ===================== FUNÇÕES  =================================*/
 
@@ -63,61 +77,69 @@ const saveNote = (note) => {
 
 };
 
-const loadNotes = () =>{
-
+const loadNotes = () => {
     let notes = localStorage.getItem('notes');
-
-    if(!notes){
-        notes = [];
-    }else{
-        notes = JSON.parse(notes);} //uso do parse: transformação do notes em forma de string para objeto
-  return notes;  
-
-}
-
-const listNotes = () =>{
-    let listNotes = loadNotes();
-    listNotes.forEach((note) => { //foreach para percorrer todos os itens do local storage
-        let divCard = document.createElement('div');
-        divCard.className = 'card';
-        divCard.style.width = '25rem';
-        let divCardBody = document.createElement('div');
-        divCardBody.className = 'cardBody';
-        divCard.appendChild(divCardBody);
-        let h5 = document.createElement('h5');
-        h5.innerText = note.title;
-        divCardBody.appendChild(h5);
-
-        let paragrafo = document.createElement('p');
-        paragrafo.innerText = note.content;
-        divCardBody.appendChild(paragrafo);
-        
-        let pData = document.createElement('p');
-        pData.innerText = "atualizado em: " +dateFormat(note.lasTime);
-
-        divCardBody.appendChild(pData);
-        notes.appendChild(divCard);
-
-        divCard.addEventListener('click', (evt) =>{
-            showNote(note);
-        })
+    if (!notes) {
+      notes = [];
+    } else {
+      notes = JSON.parse(notes);
+    }
+    notes.forEach((note) => {
+      note.lastTime = new Date(note.lastTime).getTime(); // converte o timestamp para um número
     });
+    return notes;
+  }
 
-}
-const showNote = (note) =>{
+  const listNotes = () => {
+    let listNotes = loadNotes();
+
+    listNotes.forEach((note) => { //for each utilizad0 para percorrer todos os itens do local storage
+
+    //criação da div através do js
+      let divCard = document.createElement('div');
+      divCard.className = 'card';
+      divCard.style.width = '25rem';
+      let divCardBody = document.createElement('div');
+      divCardBody.className = 'cardBody';
+      divCard.appendChild(divCardBody);
+      let h5 = document.createElement('h5');
+      h5.innerText = note.title;
+      divCardBody.appendChild(h5);
+  
+      let paragrafo = document.createElement('p');
+      paragrafo.innerText = note.content;
+      divCardBody.appendChild(paragrafo);
+  
+      let pData = document.createElement('p');
+      pData.innerText = "atualizado em: " + dateFormat(note.lastTime);
+      divCardBody.appendChild(pData);
+      notes.appendChild(divCard);
+  
+      divCard.addEventListener('click', (evt) => {
+        showNote(note);
+      })
+    });
+  };
+
+
+const showNote = (note) => {
     notes.style.display = 'none';
     addNote.style.display = 'none';
     modalView.style.display = 'block';
-    document.querySelector('#title-note').innerHTML = "<h1>"+note.title+"</h1>";
+    document.querySelector('#title-note').innerHTML = "<h1>" + note.title + "</h1>";
     document.querySelector('#content-note').innerHTML = `<p>${note.content}</p> <p>Ultima alteração: 
-    ${dateFormat(note.lasTime)}</p>`
-};
+      ${dateFormat(new Date(note.lastTime).getTime())}</p>`
+  };
 
 
 const dateFormat = (timestamp) =>{
     let date = new Date(timestamp); //converte para data
     date = date.toLocaleDateString("pt-BR");
     return date;
+};
+
+const editNote = () =>{
+    
 };
 
 listNotes();
